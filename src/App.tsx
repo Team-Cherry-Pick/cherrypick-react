66fe01157a381ed95c2ff0301298e2bb42ca1e6a
ming0o <isha3992@gmail.com>
2025-04-12 22:39:35 +0900

    Chore: RP-4 의존성 부여 및 절대 경로 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-const App = () => {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "@/pages/main-page/MainPage";
+import ProductDetailPage from "@/pages/product-detail-page/product-detail-page";
+
+function App() {
   return (
-    <>
-      Cherry Pick
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
