29f49ad28225080465b1006948651b2e39669830
ming0o <isha3992@gmail.com>
2025-04-20 18:07:11 +0900

    fix: RP-51 Jotai App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { RecoilRoot } from 'recoil';
 import GlobalStyle from './styles/global';
 import MainPage from '@/pages/main/MainPage';
 import LoginPage from '@/pages/login/LoginPage';
@@ -12,27 +11,25 @@ import { ErrorModal } from '@/components/common/ErrorModal';
 import { lightTheme, darkTheme } from '@/styles/theme';
 
 function App() {
-  const isDark = false; // 추후 상태관리 연결 예정
+    const isDark = false; // 추후 상태관리 연결 예정
 
-  return (
-    <RecoilRoot>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router>
-          <ErrorModal />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            {/* <Route path="/user" element={<UserPage />} /> */}
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/upload" element={<ProductUploadPage />} />
-            <Route path="/error" element={<ErrorPage />} />
-            <Route path="*" element={<Navigate to="/error?code=404" replace />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </RecoilRoot>
-  );
+    return (
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <GlobalStyle />
+            <Router>
+                <ErrorModal />
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    {/* <Route path="/user" element={<UserPage />} /> */}
+                    <Route path="/product/:id" element={<ProductDetailPage />} />
+                    <Route path="/upload" element={<ProductUploadPage />} />
+                    <Route path="/error" element={<ErrorPage />} />
+                    <Route path="*" element={<Navigate to="/error?code=404" replace />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
