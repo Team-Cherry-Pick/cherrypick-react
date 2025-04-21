cd118a9ecaf8c87c799f2d95ea6eea659b638e9d
ming0o <isha3992@gmail.com>
2025-04-21 17:06:48 +0900

    Chore: RP-53 로딩+파비콘 수정

---
+++ src/components/common/Loading/LoadingSpinner.tsx
@@ -0,0 +1,42 @@
+import styled, { keyframes } from 'styled-components';
+
+interface LoadingSpinnerProps {
+    message?: string;
+}
+
+export function LoadingSpinner({ message }: LoadingSpinnerProps) {
+    return (
+        <Wrapper>
+            <Spinner />
+            {message && <LoadingText>{message}</LoadingText>}
+        </Wrapper>
+    );
+}
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 32px 0;
+`;
+
+export const Spinner = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 4px solid ${({ theme }) => theme.colors.neutral[200]};
+  border-top: 4px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+`;
+
+export const LoadingText = styled.div`
+  margin-top: 16px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.content.sub};
+`;
