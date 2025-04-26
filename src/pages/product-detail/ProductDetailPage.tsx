2b3c566b60e31e12d7773a3f9c528d58466a272f
ming0o <isha3992@gmail.com>
2025-04-27 01:31:19 +0900

    Chore: RP-42 댓글 + 핫딜 추천 게시판 mock 데이터

--- src/pages/product-detail/ProductDetailPage.tsx
+++ src/pages/product-detail/ProductDetailPage.tsx
@@ -2,6 +2,9 @@ import { useParams } from "react-router-dom";
 import { mockDeals } from '@/mocks/mockDeals';
 import DefaultLayout from '@/components/layout/DefaultLayout';
 import ProductTopSection from "./ProductTopSection";
+import ProductComments from "./ProductComments";
+import ProductRecommend from "./ProductRecommend";
+import styled from "styled-components";
 
 const ProductDetailPage = () => {
     const { id } = useParams<{ id: string }>();
@@ -12,9 +15,19 @@ const ProductDetailPage = () => {
     return (
         <DefaultLayout background="board">
             <ProductTopSection deal={deal} />
+            <SubContainer >
+                <ProductRecommend />
+                <ProductComments />
+            </SubContainer>
         </DefaultLayout>
     );
 };
 
 export default ProductDetailPage;
 
+const SubContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding-top: ${({ theme }) => theme.spacing[4]};
+    gap: ${({ theme }) => theme.spacing[4]};
+`
