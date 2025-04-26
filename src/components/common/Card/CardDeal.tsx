5cf829f89043da264b3ab46fafa595801b894219
ming0o <isha3992@gmail.com>
2025-04-26 21:28:22 +0900

    Chore: RP-42 테스트용 mock 데이터 추가

--- src/components/common/Card/CardDeal.tsx
+++ src/components/common/Card/CardDeal.tsx
@@ -1,28 +1,52 @@
-// CardDeal.tsx
 import * as S from './card.style';
 import { Clock, ThumbsUp, MessageSquare } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import type { Deal } from '@/types/Deal';
 
-export const CardDeal = () => {
+interface Props {
+    deal: Deal;
+}
+
+export const CardDeal = ({ deal }: Props) => {
+    const navigate = useNavigate();
+
+    if (!deal) {
+        return null;
+    }
+
+    const mainImage = deal.imageUrls && deal.imageUrls.length > 0 ? deal.imageUrls[0].url : null;
+
+    const discountPercent =
+        deal.price && deal.price.regularPrice > 0
+            ? Math.round((1 - deal.price.discountedPrice / deal.price.regularPrice) * 100)
+            : 0;
+
     return (
-        <S.CardWrapper>
-            <S.ImageBox>{/* 이미지 */}</S.ImageBox>
+        <S.CardWrapper onClick={() => navigate(`/product/${deal.dealId}`)}>
+            <S.ImageBox>
+                {mainImage && <img src={mainImage} alt="" />}
+            </S.ImageBox>
+
             <S.InfoBox>
-                <S.Title>deal_title</S.Title>
+                <S.Title>{deal.title}</S.Title>
+
                 <S.TagRow>
-                    <S.Store>store</S.Store>
-                    <span> | </span>
-                    <S.Tags>#무료배송 #쿠폰할인 #우리우리</S.Tags>
+                    <S.Store>{deal.storeName}</S.Store>
+                    <span>|</span>
+                    <S.Tags>{deal.discountNames.map(name => `#${name}`).join(' ')}</S.Tags>
                 </S.TagRow>
+
                 <S.PriceRow>
-                    <S.Percent>NN%</S.Percent>
-                    <S.Price>NN,NNN원</S.Price>
+                    <S.Percent>{discountPercent}%</S.Percent>
+                    <S.Price>{deal.price.discountedPrice.toLocaleString()}원</S.Price>
                 </S.PriceRow>
+
                 <S.Meta>
                     <span><Clock />1시간 전</span>
                     <span className="divider">|</span>
-                    <span><ThumbsUp />111</span>
+                    <span><ThumbsUp />0</span>
                     <span className="divider">|</span>
-                    <span><MessageSquare />11</span>
+                    <span><MessageSquare />0</span>
                 </S.Meta>
             </S.InfoBox>
         </S.CardWrapper>
