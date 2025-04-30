b4cc7b5526df24673c54c020d30826431b5dcd51
don9wan <dongwan1999@gmail.com>
2025-04-30 18:13:00 +0900

    Fix: RP-43 Main Container default flex 값 설정

--- src/components/layout/DefaultLayout/index.tsx
+++ src/components/layout/DefaultLayout/index.tsx
@@ -23,20 +23,29 @@ const DefaultLayout = ({ children, background = 'root' }: DefaultLayoutProps) =>
 export default DefaultLayout;
 
 const Wrapper = styled.div<{ $background: 'root' | 'board' }>`
+  display: flex;
+  flex-direction: column;
   background-color: ${({ theme, $background }) => theme.colors.background[$background]};
   min-height: 100vh;
+
   @media (max-width: 1200px) {
     padding-inline: ${({ theme }) => theme.spacing[4]};
   }
 `;
 
 const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
   padding-top: ${({ theme }) => theme.spacing[8]};
 `;
 
 const Inner = styled.div`
   width: 100%;
   max-width: ${({ theme }) => theme.maxWidth};
+  display: flex;
+  flex-direction: column;
+  flex: 1;
   margin-inline: auto;
   padding-inline: ${({ theme }) => theme.spacing[20]};
 
