64eeb13e28a216ebca856d098ef1efcfd12e59a1
don9wan <dongwan1999@gmail.com>
2025-04-30 19:50:53 +0900

    Feat: RP-43 LoginPage 퍼블리싱

---
+++ src/pages/login/LoginBox.tsx
@@ -0,0 +1,115 @@
+// pages/login/LoginBox.tsx
+import styled from 'styled-components';
+import RepikLogo from '@/assets/icons/logo-Icon.svg';
+import KakaoLogo from '@/assets/icons/kakao-Icon.svg';
+import { textStyles } from '@/styles/global/typography';
+
+const LoginPage = () => {
+    return (
+        <LoginBoxWrapper>
+            <LogoWrapper>
+                <RepikLogoImg src={RepikLogo} alt="logo" />
+                <RepikLogoText>Repik</RepikLogoText>
+            </LogoWrapper>
+            <Title>진짜 혜택만 다시 고르다. <HighlightBold>리픽</HighlightBold></Title>
+            <Divider />
+            <Message>리픽은 <HighlightColor>1인 1계정</HighlightColor>을 원칙으로 하며<br />중복계정에 따른 업자 문제를 방지합니다.</Message>
+            <KakaoLoginButton>
+                <KakaoLoginLogoImage src={KakaoLogo} alt="kakao login" />
+                <KakaoLoginText>카카오 로그인/회원가입</KakaoLoginText>
+            </KakaoLoginButton>
+        </LoginBoxWrapper>
+    );
+};
+
+export default LoginPage;
+
+const LoginBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+  height: auto;
+  border: 1px solid ${({ theme }) => theme.colors.border.board};
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  padding: 48px 32px 32px 32px; 
+  background-color: ${({ theme }) => theme.colors.background.card};
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.content.main};
+  font-size: 2.5rem;
+  font-weight: 700;
+  line-height: 1.4;
+  cursor: pointer;
+`;
+
+const RepikLogoImg = styled.img`
+  width: ${({ theme }) => theme.spacing[12]};
+  height: ${({ theme }) => theme.spacing[12]};
+`;
+
+const RepikLogoText = styled.div`
+  padding-left: ${({ theme }) => theme.spacing[2]};
+`;
+
+const Title = styled.p`
+  ${textStyles.body}
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.content.main};
+`;
+
+const HighlightBold = styled.span`
+  font-weight: 600;
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 0.5px;
+  margin-top: 32px;
+  background-color: ${({ theme }) => theme.colors.border.board};
+`;
+
+const Message = styled.p`
+  ${textStyles.body}
+  width: 100%;
+  margin-top: 20px;
+  text-align: start;
+  color: ${({ theme }) => theme.colors.content.sub};
+`;
+
+const HighlightColor = styled.span`
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const KakaoLoginButton = styled.button`
+  width: 100%;
+  height: 48px;
+  margin-top: 16px;
+  border-radius: ${({ theme }) => theme.radius[2]};
+  background-color: #FEE502;
+  color: ${({ theme }) => theme.colors.neutral[500]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  cursor: pointer;
+  gap: 16px;
+`;
+
+const KakaoLoginLogoImage = styled.img`
+  width: ${({ theme }) => theme.spacing[6]};
+  height: ${({ theme }) => theme.spacing[6]};
+`;
+
+const KakaoLoginText = styled.p`
+  ${textStyles.subtitle}
+  margin-top: 20px;
+  text-align: start;
+  font-size: 18px;
+  color: #392020;
+`;
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,29 +1,23 @@
 // pages/login/LoginPage.tsx
 import styled from 'styled-components';
 import DefaultLayout from '@/components/layout/DefaultLayout';
-import { textStyles } from '@/styles/global/typography';
+import LoginBox from './LoginBox';
 
 const LoginPage = () => {
     return (
         <DefaultLayout>
             <Container>
-                <Title>로그인</Title>
+                <LoginBox />
             </Container>
         </DefaultLayout>
-
     );
 };
 
 export default LoginPage;
 
-const Title = styled.h1`
-    ${textStyles.heading1}
-    color: ${({ theme }) => theme.colors.content.main};
-    margin-bottom: ${({ theme }) => theme.typography.size.xl};
-`;
-
 const Container = styled.div`
-    padding: 2rem;
-    background-color: ${({ theme }) => theme.colors.background.board};
-    border: 1px solid ${({ theme }) => theme.colors.border.board};
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
 `;
