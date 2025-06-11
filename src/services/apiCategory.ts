c81871d06bc56094218dd8f8b4ac6fcc61c55fc4
Minrak Son <dungguk@naver.com>
2025-06-11 22:56:16 +0900

    Feat: RP-73 OverlayProvider를 활용한 카테고리, 스토어, 할인방식 Modal 구현
    - OverlayProvider를 사용해서 Modal을 열 수 있게 구현하고, Input, Badge, Modal 컴포넌트들을 CSS Module로 Refactor하고 기존의 파일을 삭제했습니다.

--- src/services/apiCategory.ts
+++ src/services/apiCategory.ts
@@ -3,5 +3,6 @@ import type { CategoryTree } from '@/types/Category';
 
 export async function fetchCategories(): Promise<CategoryTree> {
     const res = await apiClient.get<CategoryTree>('/category');
+    console.log(res);
     return res.data;
 }
