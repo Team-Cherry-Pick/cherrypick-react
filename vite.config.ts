482ea46c75c39e373c84d77dda0e6c559bc5bbfd
ming0o <isha3992@gmail.com>
2025-07-11 15:47:47 +0900

    Feat: 정적 파일 빌드 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { vitePluginErrorOverlay } from '@hiogawa/vite-plugin-error-overlay';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [react(), vitePluginErrorOverlay(), svgr()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: path.resolve(__dirname, 'src/assets/*'),
+                    dest: 'assets'
+                }
+            ]
+        }),
+        vitePluginErrorOverlay(), svgr()],
     server: {
         port: 3000,
     },
