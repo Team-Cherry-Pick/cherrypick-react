cdbd15d3ea7cab9c6d6832d3befa290a1b31faf4
don9wan <dongwan1999@gmail.com>
2025-09-03 01:35:41 +0900

    Design: RP-133 딜카드 모바일 대응

--- src/components/common/Card/CardDeal.tsx
+++ src/components/common/Card/CardDeal.tsx
@@ -50,27 +50,79 @@ export const CardDeal = ({ deal }: Props) => {
             <div className={styles.infoBox}>
                 <div className={styles.title}>{deal.title}</div>
 
+                {/* 모바일: 가격을 타이틀 바로 아래, 데스크탑: 기존 순서 */}
+                {isMobile && (
+                    <div className={styles.priceRow}>
+                        {deal.price.priceType === 'VARIOUS' ? (
+                            <span className={styles.variousPrice}>다양한 가격</span>
+                        ) : (
+                            <>
+                                <span className={styles.percent}>{discountPercent}%</span>
+                                <span className={styles.price}>
+                                    {deal.price.priceType === 'KRW'
+                                        ? `${formatNumber(deal.price.discountedPrice)}원`
+                                        : `$ ${formatNumber(deal.price.discountedPrice)}`}
+                                </span>
+                            </>
+                        )}
+                    </div>
+                )}
+
                 {/* 데스크탑에서 보여줄 Tag Row */}
                 <div className={styles.desktopTagRow}>
-                    <span className={styles.store}>{deal.store}</span>
-                    <span>|</span>
-                    <span className={styles.tags}>{deal.infoTags.map(name => `${name}`).join(' ')}</span>
-                </div>
-
-                <div className={styles.priceRow}>
-                    {deal.price.priceType === 'VARIOUS' ? (
-                        <span className={styles.variousPrice}>다양한 가격</span>
-                    ) : (
-                        <>
-                            <span className={styles.percent}>{discountPercent}%</span>
-                            <span className={styles.price}>
-                                {deal.price.priceType === 'KRW'
-                                    ? `${formatNumber(deal.price.discountedPrice)}원`
-                                    : `$ ${formatNumber(deal.price.discountedPrice)}`}
-                            </span>
-                        </>
+                    <span className={`${styles.tagBox} ${styles.storeTag}`}>{deal.store}</span>
+                    {deal.infoTags.length > 0 && (
+                        <span className={`${styles.tagBox} ${styles.infoTag}`}>
+                            {deal.infoTags[0]}
+                        </span>
+                    )}
+                    {deal.infoTags.length > 1 && (
+                        <span className={`${styles.tagBox} ${styles.infoTag} ${styles.remainingTags}`}>
+                            {deal.infoTags.length === 2 
+                                ? deal.infoTags[1] 
+                                : `${deal.infoTags[1]} 외 ${deal.infoTags.length - 2}`
+                            }
+                        </span>
                     )}
                 </div>
+
+                {/* 데스크탑: 가격을 기존 위치에 */}
+                {!isMobile && (
+                    <div className={styles.priceRow}>
+                        {deal.price.priceType === 'VARIOUS' ? (
+                            <span className={styles.variousPrice}>다양한 가격</span>
+                        ) : (
+                            <>
+                                <span className={styles.percent}>{discountPercent}%</span>
+                                <span className={styles.price}>
+                                    {deal.price.priceType === 'KRW'
+                                        ? `${formatNumber(deal.price.discountedPrice)}원`
+                                        : `$ ${formatNumber(deal.price.discountedPrice)}`}
+                                </span>
+                            </>
+                        )}
+                    </div>
+                )}
+
+                {/* 모바일: 태그를 메타정보 바로 위에 */}
+                {isMobile && (
+                    <div className={styles.mobileTagRow}>
+                        <span className={`${styles.tagBox} ${styles.storeTag}`}>{deal.store}</span>
+                        {deal.infoTags.length > 0 && (
+                            <span className={`${styles.tagBox} ${styles.infoTag}`}>
+                                {deal.infoTags[0]}
+                            </span>
+                        )}
+                        {deal.infoTags.length > 1 && (
+                            <span className={`${styles.tagBox} ${styles.infoTag} ${styles.remainingTags}`}>
+                                {deal.infoTags.length === 2 
+                                    ? deal.infoTags[1] 
+                                    : `${deal.infoTags[1]} 외 ${deal.infoTags.length - 2}`
+                                }
+                            </span>
+                        )}
+                    </div>
+                )}
 
                 <div className={styles.meta}>
                     <span className={styles.author} title={deal.nickname}>
