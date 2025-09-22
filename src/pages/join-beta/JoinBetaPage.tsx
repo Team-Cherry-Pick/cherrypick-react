e84bd80289a24d659b2abfe28c0a6bd061f84656
don9wan <dongwan1999@gmail.com>
2025-09-22 21:30:22 +0900

    Feat: RP-179 베타테스터 박스

--- src/pages/join-beta/JoinBetaPage.tsx
+++ src/pages/join-beta/JoinBetaPage.tsx
@@ -10,20 +10,22 @@ import BetaMainImg from '@/assets/banner/beta-main.svg';
 import BetaDescFirstImg from '@/assets/banner/beta-desc-first.svg';
 import BetaDescSecondImg from '@/assets/banner/beta-desc-second.svg';
 import BetaDescThirdImg from '@/assets/banner/beta-desc-third.svg';
+import BetaLastlyImg from '@/assets/banner/beta-lastly.svg';
 
 const JoinBetaPage = () => {
     const isMobile = useIsMobileViewport();
-    const [visibleItems, setVisibleItems] = useState<boolean[]>([false, false, false]);
+    const [visibleItems, setVisibleItems] = useState<boolean[]>([false, false, false, false]);
     const descRef1 = useRef<HTMLDivElement>(null);
     const descRef2 = useRef<HTMLDivElement>(null);
     const descRef3 = useRef<HTMLDivElement>(null);
-    const descRefs = [descRef1, descRef2, descRef3];
+    const lastlyRef = useRef<HTMLDivElement>(null);
+    const descRefs = [descRef1, descRef2, descRef3, lastlyRef];
 
     // Join-Beta 페이지에서는 항상 light 테마 유지
     useEffect(() => {
         const originalTheme = document.documentElement.getAttribute('data-theme');
         document.documentElement.setAttribute('data-theme', 'light');
-        
+
         return () => {
             // 페이지를 떠날 때 원래 테마로 복구
             if (originalTheme) {
@@ -74,28 +76,50 @@ const JoinBetaPage = () => {
                                 <img src={BetaMainImg} alt="Beta Main" className={styles.mainImage} />
                             </div>
 
+                            {/* 서비스 소개 텍스트 */}
+                            <div className={styles.serviceIntroBox}>
+                                <p className={styles.serviceIntroText}>
+                                    서비스 소개<br />
+                                </p>
+                            </div>
+
                             {/* Description 이미지들 */}
                             <div className={styles.descriptionContainer}>
-                                <div 
+                                <div
                                     ref={descRef1}
                                     className={`${styles.descriptionBox} ${visibleItems[0] ? styles.visible : ''}`}
                                 >
                                     <img src={BetaDescFirstImg} alt="Beta Description 1" className={styles.descriptionImage} />
                                 </div>
 
-                                <div 
+                                <div
                                     ref={descRef2}
                                     className={`${styles.descriptionBox} ${visibleItems[1] ? styles.visible : ''}`}
                                 >
                                     <img src={BetaDescSecondImg} alt="Beta Description 2" className={styles.descriptionImage} />
                                 </div>
 
-                                <div 
+                                <div
                                     ref={descRef3}
                                     className={`${styles.descriptionBox} ${visibleItems[2] ? styles.visible : ''}`}
                                 >
                                     <img src={BetaDescThirdImg} alt="Beta Description 3" className={styles.descriptionImage} />
                                 </div>
+                            </div>
+
+                            {/* 베타테스터 모집 텍스트 */}
+                            <div className={styles.betaRecruitBox}>
+                                <p className={styles.serviceIntroText}>
+                                    베타테스터 모집<br />
+                                </p>
+                            </div>
+
+                            {/* Beta Lastly 이미지 */}
+                            <div
+                                ref={lastlyRef}
+                                className={`${styles.lastlyBox} ${visibleItems[3] ? styles.visible : ''}`}
+                            >
+                                <img src={BetaLastlyImg} alt="Beta Lastly" className={styles.descriptionImage} />
                             </div>
                         </div>
                     </div>
