4a07b202f0c5b09361f0b01e6064b57af6b7212d
don9wan <dongwan1999@gmail.com>
2025-09-22 17:06:51 +0900

    Feat: RP-179 베타참여 페이지 레이아웃 구좌 설정

--- src/pages/join-beta/JoinBetaPage.tsx
+++ src/pages/join-beta/JoinBetaPage.tsx
@@ -1,11 +1,16 @@
 import styles from './JoinBetaPage.module.css';
 import DefaultLayout from '@/components/layout/DefaultLayout';
+import useIsMobileViewport from '@/hooks/useIsMobileViewport';
 
 const JoinBetaPage = () => {
+    const isMobile = useIsMobileViewport();
+
     return (
         <DefaultLayout background="board">
             <div className={styles.container}>
-                {/* 빈 페이지 - 추후 컨텐츠 추가 예정 */}
+                <div className={`${isMobile ? styles.joinBetaBoxMobile : styles.joinBetaBoxWrapper}`}>
+                    {/* 빈 페이지 - 추후 컨텐츠 추가 예정 */}
+                </div>
             </div>
         </DefaultLayout>
     );
